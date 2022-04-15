62438584ac236433f2974fc7d9fbb5c3b310f852
cchou-github <callmeccyng@gmail.com>
2022-04-16 08:06:53 +0900

    add user and session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,22 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery prepend: true
+
+  # protected add to the user base controller
+  # def authenticate_user!
+  #   if user_signed_in?
+  #     super
+  #   else
+  #     redirect_to root_path
+  #   end
+  # end
+
   private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+  
 
   def authorize
     redirect_to login_url, alert: "Not authorized" if current_user.nil?
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   before_create :generate_random_id
+  #TODO: password complexity
 
   has_secure_password    
   validates_uniqueness_of :account
